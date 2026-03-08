dd20d4aba0e0a711f15d7e25903f0573ca5e5201
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-08 11:06:42 +0000

    added changes in register

--- src/Routes/AuthRoutes.js
+++ src/Routes/AuthRoutes.js
@@ -43,26 +43,21 @@ Router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
-    if (!username || !email || !password) {
+    if (!username || !email || !password)
       res.status(400).json({ message: "Fields are required" });
-    }
-
-    if (password.length < 6) {
+    if (password.length < 6)
       res.status(400).json({ message: "Password length less than 6" });
-    }
-
-    if (username.length < 3) {
+    if (username.length < 3)
       res
         .status(400)
         .json({ message: "Username must be atleast 3 characters long" });
-    }
+
     const checkExistence = await User.findOne({
       $or: [{ email: email }, { username: username }],
     });
 
-    if (checkExistence) {
-      res.status(400).json({ message: "User already exist" });
-    }
+    if (checkExistence) res.status(400).json({ message: "User already exist" });
+
     // const hashedPassword = hashPassword(password);
     const avatar = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
     const user = new User({

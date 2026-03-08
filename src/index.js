7e41a8513da2a70f2c627e9cddd89eda3cab11d3
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-08 03:33:12 +0000

    added cron

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import express from "express";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import BookRoutes from "./Routes/BookRoutes.js";
+import job from "./config/cron.js";
 import { connectToDb } from "./config/db.js";
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json({}));
 app.use("/api/auth", AuthRoutes);
 app.use("/api/books", BookRoutes);
+if (process.env.NODE_ENV === "production") job.start();
 app.listen(port, () => {
   connectToDb();
 });

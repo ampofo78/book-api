cc2661e2d27a474a8217d786c5a4cb7591e9820c
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-08 15:46:04 +0000

    added changes in index

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import express from "express";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import BookRoutes from "./Routes/BookRoutes.js";
-import job from "./config/cron.js";
 import { connectToDb } from "./config/db.js";
 
 const app = express();
@@ -12,7 +11,7 @@ app.use(cors());
 app.use(express.json({}));
 app.use("/api/auth", AuthRoutes);
 app.use("/api/books", BookRoutes);
-if (process.env.NODE_ENV === "production") job.start();
+//if (process.env.NODE_ENV === "production") job.start();
 app.listen(port, () => {
   connectToDb();
 });

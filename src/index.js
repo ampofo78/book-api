7d5c8af026682a769ed9d3b67e3a8d49c279aaa9
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-09 00:52:38 +0000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import job from "./config/cron.js";
+//import job from "./config/cron.js";
 import { connectToDb } from "./config/db.js";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import BookRoutes from "./Routes/BookRoutes.js";
 const app = express();
-if (process.env.NODE_ENV === "production") job.start();
+//if (process.env.NODE_ENV === "production") job.start();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({}));
@@ -16,7 +16,5 @@ app.get("/api/health", (req, res) => {
   return res.status(200).json({ status: "ok" });
 });
 app.listen(port, () => {
-  connectToDb().then(() => {
-    console.log("Connected to db");
-  });
+  connectToDb();
 });

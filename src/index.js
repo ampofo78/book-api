b076b4efecbcd21fc7c05f2b7099ef8485e2ddc4
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-09 01:24:15 +0000

    added new thing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import cors from "cors";
 import "dotenv/config";
 import express from "express";
-//import job from "./config/cron.js";
+import job from "./config/cron.js";
 import { connectToDb } from "./config/db.js";
 import AuthRoutes from "./Routes/AuthRoutes.js";
 import BookRoutes from "./Routes/BookRoutes.js";
 const app = express();
-//if (process.env.NODE_ENV === "production") job.start();
+if (process.env.NODE_ENV === "production") job.start();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json({}));

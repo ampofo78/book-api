b7c2bc93018a5602b04bfc0b932d25ffdbc5317e
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-11 13:02:09 +0000

    changes in AuthTokenVerifier

--- src/Middleware/AuthTokenVerifier.js
+++ src/Middleware/AuthTokenVerifier.js
@@ -3,14 +3,15 @@ import jwt from "jsonwebtoken";
 
 export const VerifyToken = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split()[1];
-    const user = jwt.verify(token, process.env.SECRET_KEY, {});
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      return res.status(401).json({ message: "Unauthorized User" });
-    }
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      } else {
+        req.user = user;
+        next();
+      }
+    });
   } catch (error) {
     console.log(error);
   }

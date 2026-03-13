2ad4c2dd65dee7f52a007e6908036491a3a11d87
Ampofo Oduro <Ampofoodurodaniel@gmail.com>
2026-03-13 14:38:44 +0000

    changes in json

--- src/index.js
+++ src/index.js
@@ -9,10 +9,15 @@ const app = express();
 if (process.env.NODE_ENV === "production") job.start();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json({}));
+app.use(
+  express.json({
+    limit: "20mb",
+  }),
+);
 app.use(
   express.urlencoded({
     extended: true,
+    limit: "20mb",
   }),
 );
 app.use("/api/auth", AuthRoutes);
